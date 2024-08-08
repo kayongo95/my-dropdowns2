352acfd4766e9b9346e65f87670b2c210ece6cf5
Kayongo Nicholas <nicklasoncrystal@gmail.com>
2024-08-09 00:44:34 +0300

    dropdown2
    my dropdown2

---
+++ dropdowns2.js
@@ -0,0 +1,45 @@
+function Menu() {
+    const menu = document.getElementById("drop-menu");
+
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+function About() {
+    const about = document.getElementById('drop-menu1');
+
+    if (about.style.display === 'block') {
+        about.style.display = 'none';
+    } else {
+        about.style.display = 'block';
+    }
+}
+
+function Contact() {
+    const contact = document.getElementById('drop-menu3');
+
+    if (contact.style.display === 'block') {
+        contact.style.display = 'none';
+    } else {
+        contact.style.display = 'block';
+    }
+}
+
+function Gallery() {
+    const gallery = document.getElementById('drop-menu2');
+
+    if (gallery.style.display === 'block') {
+        gallery.style.display = 'none';
+    } else {
+        gallery.style.display = 'block';
+    }
+}
+
+
+
+
+
+
